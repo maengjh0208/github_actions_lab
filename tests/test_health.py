b7a1aed27ef8d17012347d5ff2e77fcf1c211be4
maengjh <juhee.bda@gmail.com>
2026-06-29 18:07:35 +0900

    test: GET /health API 응답값 변경후 staging, production 서버에서 정상적으로 배포되는지 테스트

--- tests/test_health.py
+++ tests/test_health.py
@@ -10,4 +10,4 @@ def test_health():
     response = client.get("/health")
 
     assert response.status_code == 200
-    assert response.json()["status"] == "ok"
+    assert response.json()["status"] == "!!!! ok !!!!"
